71a9e95ddee4497e80cceaa85700a6b1f52e3154
arushi1811 <arushig@bu.edu>
2021-09-10 23:14:03 -0400

    Sign up html css done

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,12 +1,21 @@
+import "./SignUp.css"
+import {Link} from "react-router-dom"
+
 const SignUp = () => {
     return (
-        <div>
-            <label htmlFor="email">Email *</label>
-            <input type="text" name="email" value=""/>
-            <label htmlFor="password">Password *</label>
-            <input type="password" name="password" value=""/>
-            <label htmlFor="retype">Retype Password *</label>
-            <input type="password" name="retype" value=""/>
+        <div className="signup">
+            <form>
+                <h1>Sign up</h1>
+                <input type="text" name="email" value="" placeholder="example@email.com"/>
+                <input type="password" name="password" value="" placeholder="*********"/>
+                <button type="submit">Sign up</button>
+                <p>By continuing, you agree to The Terms of Service and Privacy Statement.</p>
+            </form>
+            <div className="line"></div>
+            <div className="signedup">
+                <p>Have an account?</p>
+                <Link to="/login">Log In</Link>
+            </div>
         </div>
     )
 }
