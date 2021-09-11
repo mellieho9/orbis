a00b80d0a9d15ce6ec7ae6df27402c5652664fbb
Aliyah Qayum <qayumaliyah@gmail.com>
2021-09-11 00:41:33 -0400

    front end for login pagee

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,11 +1,29 @@
+import "./Login.css"
+import {Link} from "react-router-dom"
+
 const Login = () => {
     return (
-        <div>
-            <label htmlFor="email">Email *</label>
+      
+ <div className = "loginWrapper">
+           <div className = "login">
+            <h1>Orbis</h1>
+            <h2>Enter motto here</h2>
             <input type="text" name="email" value=""/>
-            <label htmlFor="password">Password *</label>
             <input type="password" name="password" value=""/>
+            <button>Login</button>
+            
+            <div className="line">
+
+            </div>
+
+            <div className = "notLogin">
+                <p>No account?</p>
+                <Link to = "/signup">Sign Up</Link>
+
+            </div>
         </div>
+ </div>
+
     )
 }
 
