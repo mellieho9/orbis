a6030deb52adbdc24524813924a27cbb835b1e1e
arushi1811 <arushig@bu.edu>
2021-09-10 23:33:03 -0400

    added css for Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,8 +1,11 @@
 import {Link} from "react-router-dom"
+import logo from "../../logo.svg"
+import "./Navbar.css"
 
 const Navbar = () => {
     return (
         <nav>
+            <img src={logo} alt="logo" height="60px" width="60px" />
             <ul>
                 <Link to="/login"><li>Login</li></Link>
                 <Link to="/signup"><li>Get started</li></Link>
