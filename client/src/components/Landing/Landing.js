5e2982272ff7fc7824927f4b0cc461a932c3057b
Aliyah Qayum <qayumaliyah@gmail.com>
2021-09-11 13:26:28 -0400

    front end for landing page in progress

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,8 +1,30 @@
+import "./Landing.css"
+
+
 const Landing = () => {
     return (
-        <div className="landing">
-            
-        </div>
+   
+        <div className = "landing">
+         <button>
+             <h1>Be Productive</h1>
+             <h6>Using our pomodoro timer, you can accomplish what you want to do in the way you want</h6>
+         </button>
+
+         <button>
+             <h1>Study With Friends</h1>
+             <h6>Create a room, select games, set background music, and invite your friends for your DREAM study session</h6>
+         </button>
+
+         <button>
+             <h1>Get Motivated</h1>
+             <h6>Check out our motivational corner for study inspo, tips, and our Orbis community!</h6>
+         </button>
+
+   
+     </div>
+
+
+
     )
 }
 
